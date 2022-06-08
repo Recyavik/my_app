f28f712b5d76eae95cc6d046cb7c25e2ba73fb2a
Viktor Kataev <v_kataev@mail.ru>
2022-06-08 18:47:42 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_app/my_icons_v.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: "Flutter приложение",
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFFAE03E2),
+            title: const Text("Заголовок"),
+            centerTitle: true,
+          ),
+          body: IconButton(
+            icon: const Icon(
+              My_icons_v.i2expertcommission,
+            ),
+            color: const Color.fromARGB(255, 246, 246, 246),
+            iconSize: 50.0,
+            onPressed: () {
+              log('Press');
+            },
+          ),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: const Color.fromARGB(224, 5, 78, 95),
+            splashColor: const Color.fromARGB(229, 100, 219, 3),
+            onPressed: () {},
+            child: const Text("+"),
+          ),
+          backgroundColor: const Color(0xE189CBF3),
+        ),
+      ),
+    );
