796c3ed987aacdf86e039d1f4030e54ca965a1ce
Viktor Kataev <v_kataev@mail.ru>
2022-06-08 19:42:28 +0800

    delete

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:my_app/my_icons_v.dart';
 
 void main() => runApp(
